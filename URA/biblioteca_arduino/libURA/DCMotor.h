8a818aa6b536e02304600297c78880170b23ae87
William Ribeiro <william-cribeiro@bct.ect.ufrn.br>
2017-03-15 11:38:55 -0300

    Update DCMotor.h

--- URA/biblioteca_arduino/libURA/DCMotor.h
+++ URA/biblioteca_arduino/libURA/DCMotor.h
@@ -8,15 +8,15 @@ class DCMotor
 {
   public:
     DCMotor(int in1, int in2, int in3, int in4);
-    void forward();
-    void backward();
-    void left();
-    void right(); 
-    void stop(); 
+    void forward();     // Andar para frente
+    void backward();    //Andar para tras 
+    void left();        // Esquerda 
+    void right();       //Direita
+    void stop();        //Parada
 
   private:
-    void clockwise(int inx, int iny);
-    void antiClockwise(int inx, int iny); 
+    void clockwise(int inx, int iny);   //Faz o motor girar no sentido horário
+    void antiClockwise(int inx, int iny);     //Faz o motor girar no sentido anti-horario 
     int _in1, _in2, _in3, _in4; 
     //const int  IN1 = 5, IN2 = 4, IN3 = 3, IN4 = 2;//pinos
   
